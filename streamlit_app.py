4664014d72bfeedf9f0d3db3034daac6659ee473
Victor <vicky.robilson5528@gmail.com>
2025-03-10 14:34:50 +0100

    updated page title

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,13 @@ from dotenv import load_dotenv
 
 # load this environment variable 
 load_dotenv()
-
+st.set_page_config(page_title='Gemini API Chatbot')
 # initialize env var from .env
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 # Check if the api key is working properly
 if not GEMINI_API_KEY:
-    print("gemini key api is not set")
+    st.warning("gemini key api is not set")
     exit()
 
 client = genai.Client(api_key=GEMINI_API_KEY)
@@ -32,7 +32,7 @@ if st.button(label="Submit"):
         
         st.write(response.text)
     except ValueError as e:
-        st.write(f"Error occured {e}")
+        st.warning(f"Error occured {e}")
 
 st.sidebar.subheader("Chat History")
 if st.session_state.chat_history:
